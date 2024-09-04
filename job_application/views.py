bf42adcc5574feb847b746514d50666fe70bc8e6
renganomics <orengasi@gmail.com>
2024-09-04 01:11:34 +0100

    Added code to store data to database and print success message

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
 
 def index(request):
@@ -11,5 +13,10 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+
+            messages.success(request, "Form submitted successfully!")
 
     return render(request, "index.html")
