2cf586269044cfc6e564cfa86512158513d483aa
renganomics <orengasi@gmail.com>
2024-09-04 10:32:04 +0100

    Created base template and about page

--- job_application/views.py
+++ job_application/views.py
@@ -27,3 +27,7 @@ def index(request):
             messages.success(request, "Form submitted successfully!")
 
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
